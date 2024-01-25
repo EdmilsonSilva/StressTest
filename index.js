b08fb904ddfde107aee5b5932862ecb4df339f73
Edmilson Silva <ed1000sons@gmail.com>
2024-01-25 00:42:30 +0000

    feat: updates

--- index.js
+++ index.js
@@ -1,20 +1,36 @@
-const express = require("express");
+import express from "express";
+import bodyParser from "body-parser";
+import { nanoid } from "nanoid";
 const app = express();
 const port = 27500;
-const bodyParser = require("body-parser");
 // app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
-// app.use(bodyParser.json())
+app.use(bodyParser.json())
 app.use(bodyParser.raw());
 
+const policies = [];
+
 app.all("/policy", (_, res) => {
+  const policyNumber = `ED1000POLICY${nanoid()}`
+  policies.push(policyNumber);
   res.json({
-    policyNumber:
-      "ED1000POLICY" +
-      Math.floor(Math.random() * 1500000)
-        .toString()
-        .padStart(10, "0")
+    policyNumber,
+    size: policies.length
   });
+  console.info(`Policies Issued: ${policies.length}`);
+});
+app.all("/:policyId", (req, res) => {
+  const { policyId } = req.params;
+  const indexPolicy = policies.findIndex(v => v == policyId);
+  if (indexPolicy == -1) {
+    return res.sendStatus(404);
+  }
+  const [policyCallback] = policies.splice(indexPolicy, 1);
+  res.json({
+    policyCallback,
+    size: policies.length
+  });
+  console.warn(`Policies Callback: ${policies.length}`);
 });
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
