f42d35b76f11f53acf03793e576cd03a18e0da37
Edmilson Silva <ed1000sons@gmail.com>
2024-02-09 11:14:48 +0000

    feat: timedout requests 25secs

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 import express from "express";
 import bodyParser from "body-parser";
 import { nanoid } from "nanoid";
+import timeout from "connect-timeout";
 const app = express();
 const port = 27500;
 // app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 app.use(bodyParser.raw());
+app.use(timeout('25s'));
 
 const Reset = "\x1b[0m"
 const FgGreen = "\x1b[32m"
@@ -16,11 +18,16 @@ const policies = [];
 let policyAmount = 1;
 let callbackAmount = 1;
 
-app.all("/policy", (_, res) => {
+app.all("/policy", (req, res, next) => {
+  if (req.timedout) {
+    return next();
+  }
   const policyNumber = `ED1000POLICY${nanoid()}`
   policies.push(policyNumber);
   res.json({
     policyNumber,
+    data: "X".repeat(1043916),
+    //1048576
     size: policies.length
   });
   console.info(`${FgYellow}Issued: ${policyAmount++} - ${policyNumber} Total: ${policies.length}${Reset}`);
