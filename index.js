c057518ed6bae6d4cffe0213740e30943484d571
Edmilson Silva <ed1000sons@gmail.com>
2024-11-11 09:17:24 +0000

    feat: sync and async process

--- index.js
+++ index.js
@@ -21,60 +21,57 @@ const FgBlue = "\x1b[34m"
 const policies = [];
 let policyAmount = 1;
 let callbackAmount = 1;
-let attempts = 0;
 
 app.post("/policy", (req, res, next) => {
   if (req.timedout) {
     return next();
   }
-  const random = Math.floor(Math.random() * 100) + 1;
+  const random = Math.floor(Math.random() * 10) + 1;
   const { callback_url, status, street } = req.body;
-  // if (attempts < 2) {
-  //   attempts++;
-  //   const interval1 = setInterval(() => { console.count('attempts') }, 1000);
-  //   setTimeout(() => {
-  //     clearInterval(interval1);
-  //     console.countReset('attempts');
-  //     return res.sendStatus(429);
-  //   }, 5000);
-  //   return;
-  // }
-  const even = random % 2 === 0;
-  const odd = random % 2 === 1;
-  if (odd) {
+  const case1 = [2,3,4,5,6,7,8,9].includes(random);
+  const case2 = [10].includes(random);
+  const case3 = [1].includes(random);
+  console.info("case1:", case1, "case2:", case2,"case3:", case3);
+  if (case3) {
     setTimeout(() => {
-      res.status(200).json({status, street});
+      res.status(400).json({status, street});
     }, 3000);
   } else {
-    const policyNumber = `ED1000POLICY${nanoid()}`;
-    const interval = setInterval(() => { console.count(policyNumber) }, 1000);
-    setTimeout(() => {
-      clearInterval(interval);
-      console.countReset(policyNumber);
-      policies.push(policyNumber);
-      const body = {
-        policyNumber: even ? policyNumber : void 0,
-        orderId: even ? policyNumber : void 0,
-        callback_url,
-        // data: "X".repeat(1043916),
-        //1048576
-        status,
-        street,
-        size: policies.length
-      };
-      res.status(200).json(body);
-      console.info(`${FgYellow}Issued: ${policyAmount++} - ${policyNumber} Total: ${policies.length}${Reset}`);
-      const options = {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(body),
-      }
-      if (callback_url) {
-        setTimeout(() => {
-          fetch(`${callback_url}`, options);
-        }, 3000);
-      }
-    }, 6000);
+    if (case2) {
+      setTimeout(() => {
+        res.status(429).json({status, street});
+      }, 3000);
+    } else {
+      const policyNumber = `ED1000POLICY${nanoid()}`;
+      const interval = setInterval(() => { console.count(policyNumber) }, 1000);
+      setTimeout(() => {
+        clearInterval(interval);
+        console.countReset(policyNumber);
+        policies.push(policyNumber);
+        const body = {
+          policyNumber: case1 ? policyNumber : void 0,
+          orderId: case1 ? policyNumber : void 0,
+          callback_url,
+          // data: "X".repeat(1043916),
+          //1048576
+          status,
+          street,
+          size: policies.length
+        };
+        res.status(200).json(body);
+        console.info(`${FgYellow}Issued: ${policyAmount++} - ${policyNumber} Total: ${policies.length}${Reset}`);
+        const options = {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify(body),
+        }
+        if (callback_url) {
+          setTimeout(() => {
+            fetch(`${callback_url}`, options);
+          }, 3000);
+        }
+      }, 6000);
+    }
   }
 });
 app.all("/cb5d8aa6-c9f4-4517-87d9-3a92a2fc1262", (req, res) => {
@@ -115,12 +112,11 @@ app.all("/callback/:processId", (req, res) => {
       setTimeout(()=> {
         fetch(`${phs}/policy/callback/${processId}`, options);
       }, 3000);
+    } else {
+     fetch(`https://27500-edmilsonsilv-stresstest-pu1oq4oaxpr.ws-eu116.gitpod.io/cb5d8aa6-c9f4-4517-87d9-3a92a2fc1262`, options);
     }
-    // else {
-    //  fetch(`https://27500-edmilsonsilv-stresstest-pu1oq4oaxpr.ws-eu116.gitpod.io/cb5d8aa6-c9f4-4517-87d9-3a92a2fc1262`, options);
-    // }
     setTimeout(()=> {
-      fetch(`https://webhook.site/05f7fce3-004e-4ab4-96e8-1d5014c19121/${processId}`, options);
+      fetch(`https://webhook.site/c74827f5-5199-48ac-b4ed-85993de1166f/${processId}`, options);
     }, 3000);
     res.status(200).json({ status: 200, success: true, message: "" });
   }, 7000);
